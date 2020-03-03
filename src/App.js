bf91cab37a981d5723e8fe7a205af5ea6ff67cdf
sukibeww <sukialiong@gmail.com>
2020-03-04 00:36:38 +1100

    infinite spining 3d ring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react'
+import {
+  Illustration,
+  Ellipse,
+  Shape,
+  useRender,
+} from 'react-zdog'
+import { a, useSpring } from '@react-spring/zdog'
 
-function App() {
+const TAU = Math.PI * 2
+
+function Guy() {
+  // Change motion every second
+  const [up, setUp] = useState(true)
+  useEffect(() => void setInterval(() => setUp(previous => !previous), 450), [])
+  // Turn static values into animated values
+  const { rotation } = useSpring({
+    rotation: up ? 0 : Math.PI,
+  })
+  // useRender allows us to hook into the render-loop
+  const ref = useRef()
+  let t = 0
+  useRender(() => (ref.current.rotate.y += 0.05))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Shape ref={ref} path={[{ x: -3 }, { x: 3 }]} stroke={0} color="#747B9E">
+      <a.Anchor rotate={rotation.interpolate(r => ({ x: TAU / 18 + -r / 4 }))}>
+        <Ellipse diameter={80} translateZ={40} stroke={20} color="#ffffff" />
+      </a.Anchor>
+    </Shape>
+  )
 }
 
-export default App;
+const App = () => {
+  return (
+    <Illustration dragRotate={true} zoom={8}>
+      <Guy />
+    </Illustration>
+  )
+}
+
+export default App
